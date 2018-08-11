01143d8be02f86b0dec03792f0b31155586e2683
azibit <abdulazeezidris28@gmail.com>
2018-08-11 08:00:24 +0100

    Update

--- predictor.py
+++ predictor.py
@@ -12,7 +12,7 @@ classifier = load_model('classifier.h5')
 train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
 training_set = train_datagen.flow_from_directory('training_set', target_size = (64, 64), batch_size = 32, class_mode = 'binary')
 
-test_image = image.load_img('cat1.jpg', target_size = (64, 64))
+test_image = image.load_img('cat2.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 
 test_image = np.expand_dims(test_image, axis = 0)
